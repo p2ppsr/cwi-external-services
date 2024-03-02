820d16f01b55511da65a14f33a5db359a2301f31
Tone <tone@kizmet.org>
2024-03-02 08:59:20 -0500

    more test cleanup. runs in 9s. all pass

--- test/getMerkleProof.test.ts
+++ test/getMerkleProof.test.ts
@@ -1,3 +1,4 @@
+import { wait } from "cwi-base";
 import { CwiExternalServices } from "../src";
 import { getMerkleProofFromGorillaPool, getMerkleProofFromMetastreme, getMerkleProofFromTaal, getMerkleProofFromWhatsOnChain } from "../src/getMerkleProofV1";
 import { verifyDojoTransactionProof } from "./verifyDojoTransactionProofs";
@@ -6,6 +7,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 describe("getMerkleProof", () => {
+    jest.setTimeout(900000)
     const mainTaalApiKey = process.env.MAIN_TAAL_API_KEY || ''
     const testTaalApiKey = process.env.TEST_TAAL_API_KEY || ''
     const mainDojoConnection = process.env.MAIN_DOJO_CONNECTION || ''
@@ -31,7 +33,7 @@ describe("getMerkleProof", () => {
     
     const orphanedTxid = 'b37a0afaad97930f2b08b080557dbea5e49bbb78fba78235c57d4acdd97c3054'
 
-    test.skip("CwiExternalServices main d5ec76e61c6a035b85bdc58cb179da3570417459b1dd7b012088a97e2901a83b", async () => {
+    test.skip("00_CwiExternalServices main d5ec76e61c6a035b85bdc58cb179da3570417459b1dd7b012088a97e2901a83b", async () => {
         const opts = CwiExternalServices.createDefaultOptions()
 
         const services = new CwiExternalServices(opts)
@@ -42,68 +44,69 @@ describe("getMerkleProof", () => {
 
         expect(r).toBeTruthy()
 
-    }, 9000000)
+    })
 
-    test.skip("WoC orphanedTxid", async () => {
+    test.skip("01_WoC orphanedTxid", async () => {
         const r = await getMerkleProofFromWhatsOnChain(orphanedTxid, 'test')
         // Transaction was mined in an orphaned block as well as an active block
         // Result should actually be multiple proofs
         expect(r).toBeTruthy()
-    }, 400000)
+    })
 
-    test.skip("verify dojo transaction proofs", async () => {
+    test.skip("02_verify dojo transaction proofs", async () => {
         const r = await verifyDojoTransactionProof(mainDojoConnection, mainTaalApiKey, false)
 
         expect(r.disagreement).toBe(0)
         expect(r.failedCount).toBe(0)
 
-    }, 400000)
+    })
 
-    test.skip("verify staging_dojo transaction proofs", async () => {
+    test.skip("03_verify staging_dojo transaction proofs", async () => {
         const r = await verifyDojoTransactionProof(testDojoConnection, testTaalApiKey, false)
 
         expect(r.disagreement).toBe(0)
         expect(r.failedCount).toBe(0)
 
-    }, 400000)
+    })
 
-    test("taal unknown txid", async () => {
+    test("04_taal unknown txid", async () => {
             const p = await getMerkleProofFromTaal(unknownTxid, testTaalApiKey)
             expect(p).not.toBeTruthy()
-    }, 100000)
+    })
 
-    test.skip("gorillapool mainNet txid 9c31ed1dea4ec1aae0475addc0a74eaed68b718d9983d42b111c387d6696a949", async () => {
+    test.skip("05_gorillapool mainNet txid 9c31ed1dea4ec1aae0475addc0a74eaed68b718d9983d42b111c387d6696a949", async () => {
             const p = await getMerkleProofFromGorillaPool('9c31ed1dea4ec1aae0475addc0a74eaed68b718d9983d42b111c387d6696a949')
             expect(p).toBeTruthy()
             expect(p?.index).toBe(10)
-    }, 100000)
+    })
     
-    test("taal testNet txid d542b413afe17d5838e2ae5b7d47441a6d61eaeaa13f709e39133adbfc2ef19b", async () => {
-            const p = await getMerkleProofFromTaal('d542b413afe17d5838e2ae5b7d47441a6d61eaeaa13f709e39133adbfc2ef19b', testTaalApiKey)
-            expect(p).toBeTruthy()
-            expect(p?.index).toBe(1)
-    }, 100000)
+    test("06_taal testNet txid d542b413afe17d5838e2ae5b7d47441a6d61eaeaa13f709e39133adbfc2ef19b", async () => {
+        const p = await getMerkleProofFromTaal('d542b413afe17d5838e2ae5b7d47441a6d61eaeaa13f709e39133adbfc2ef19b', testTaalApiKey)
+        expect(p).toBeTruthy()
+        expect(p?.index).toBe(1)
+    })
 
-    test("taal testNet txid fde05190cb57b4906a98788bea6c1c127af98f014d9ca155948c1431b35b8361", async () => {
-            const p = await getMerkleProofFromTaal('fde05190cb57b4906a98788bea6c1c127af98f014d9ca155948c1431b35b8361', testTaalApiKey)
-            expect(p).toBeTruthy()
-            expect(p?.index).toBe(10)
-    }, 100000)
+    test("07_taal testNet txid fde05190cb57b4906a98788bea6c1c127af98f014d9ca155948c1431b35b8361", async () => {
+        const p = await getMerkleProofFromTaal('fde05190cb57b4906a98788bea6c1c127af98f014d9ca155948c1431b35b8361', testTaalApiKey)
+        expect(p).toBeTruthy()
+        expect(p?.index).toBe(10)
+    })
 
-    test("WhatsOnChain testNet txid fde05190cb57b4906a98788bea6c1c127af98f014d9ca155948c1431b35b8361", async () => {
-            const p = await getMerkleProofFromWhatsOnChain('fde05190cb57b4906a98788bea6c1c127af98f014d9ca155948c1431b35b8361', 'test')
-            expect(p).toBeTruthy()
-            expect(p?.index).toBe(10)
-    }, 100000)
+    test("08_WhatsOnChain testNet txid fde05190cb57b4906a98788bea6c1c127af98f014d9ca155948c1431b35b8361", async () => {
+        const p = await getMerkleProofFromWhatsOnChain('fde05190cb57b4906a98788bea6c1c127af98f014d9ca155948c1431b35b8361', 'test')
+        expect(p).toBeTruthy()
+        expect(p?.index).toBe(10)
+    })
 
-    test("Metastreme testNet txid fde05190cb57b4906a98788bea6c1c127af98f014d9ca155948c1431b35b8361", async () => {
-            const p = await getMerkleProofFromMetastreme('fde05190cb57b4906a98788bea6c1c127af98f014d9ca155948c1431b35b8361', 'test')
-            expect(p).toBeTruthy()
-            expect(p?.index).toBe(10)
-    }, 100000)
+    test("09_Metastreme testNet txid fde05190cb57b4906a98788bea6c1c127af98f014d9ca155948c1431b35b8361", async () => {
+        const p = await getMerkleProofFromMetastreme('fde05190cb57b4906a98788bea6c1c127af98f014d9ca155948c1431b35b8361', 'test')
+        expect(p).toBeTruthy()
+        expect(p?.index).toBe(10)
+    })
 
-    test("taal all 12 txs from a testnet block", async () => {
+    test.skip("10_taal all 12 txs from a testnet block", async () => {
         for (let i = 0; i < txids.length; i++) {
+            await wait(200) // Not too fast to avoid annoying the service.
             try {
                 const p = await getMerkleProofFromTaal(txids[i], testTaalApiKey)
                 expect(p).toBeTruthy()
@@ -112,12 +115,12 @@ describe("getMerkleProof", () => {
                 console.log(`taal failed on i=${i}`)
                 throw err
             }
-            await new Promise(f => setTimeout(f, 500)) // Not too fast to avoid annoying the service.
         }
-    }, 100000)
+    })
 
-    test("WhatsOnChain all 12 txs from a testnet block", async () => {
+    test.skip("11_WhatsOnChain all 12 txs from a testnet block", async () => {
         for (let i = 0; i < txids.length; i++) {
+            await wait(200) // Not too fast to avoid annoying the service.
             const p = await getMerkleProofFromWhatsOnChain(txids[i], 'test')
             try {
                 expect(p).toBeTruthy()
@@ -126,12 +129,12 @@ describe("getMerkleProof", () => {
                 console.log(`woc failed on i=${i}`)
                 throw err
             }
-            await new Promise(f => setTimeout(f, 500)) // Not too fast to avoid annoying the service.
         }
-    }, 100000)
+    })
 
-    test("Metastreme all 12 txs from a testnet block", async () => {
+    test.skip("12_Metastreme all 12 txs from a testnet block", async () => {
         for (let i = 0; i < txids.length; i++) {
+            await wait(200) // Not too fast to avoid annoying the service.
             const p = await getMerkleProofFromMetastreme(txids[i], 'test')
             try {
                 expect(p).toBeTruthy()
@@ -140,7 +143,6 @@ describe("getMerkleProof", () => {
                 console.log(`metastreme failed on i=${i}`)
                 throw err
             }
-            await new Promise(f => setTimeout(f, 500)) // Not too fast to avoid annoying the service.
         }
-    }, 100000)
+    })
 })
